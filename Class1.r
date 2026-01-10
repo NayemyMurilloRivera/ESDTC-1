910a79faee4fdfb341264afa76b302b61ec475de
Programmer rabbit <154576550+NayemyMurilloRivera@users.noreply.github.com>
2026-01-10 07:40:05 -0500

    Add pie chart for study level distribution
    Added pie chart and legend for study level distribution.

--- Class1.r
+++ Class1.r
@@ -13,3 +13,12 @@ tabla
 barplot(fi)
 gb=barplot(fi, main="Nivel de estudio", col=c("yellow", "red","blue", "green"), name=c("Sin estudios", "Estudios primarios", "Estudios secundarios", "Estudios superiores"), ylim=c(0, max(fi)*1.1))
 text(x=gb, y=fi, labels=fi, pos=3) #print?
+# Frecuencia  relativa Percentual       
+porcentaje<-c(15,35,30,20)
+etiqueta<-c(paste(porcentaje,"%",sep=""))
+colores<-c("green","yellow","red","blue")
+pie(porcentaje,labels=etiqueta,cloCkwise=TRUE,col=c("green","yellow","red","blue"), main = "Diagrama circular del Nivel de Estudio de los trabajadores")
+legend("bottomright",c("Sin estudios", "Estudios primarios", "Estudios Secundarios", "Estudios superiores"), cex=0.6,fill=colores)
+#puede moverse de ubicación la leyenda
+legend("topright",c("Sin estudios", "Estudios primarios", "Estudios Secundarios", "Estudios superiores"), cex=0.6,fill=colores)
+
